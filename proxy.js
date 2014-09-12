9f36e7769a97c2ee6605dfd7455e1cbcee42ccb9
jmorille <jmorille@gmail.com>
2014-09-12 10:50:41 +0200

    Proxy

--- proxy.js
+++ proxy.js
@@ -5,6 +5,9 @@ var express = require("express"),
     methodOverride = require('method-override'),
     port = parseInt(process.env.PORT, 10) || 8000;
 
+// https://github.com/senchalabs/connect#middleware
+var compression = require('compression');
+
 // https://github.com/strongloop/express/tree/master/examples
 
 app.use(methodOverride());
@@ -13,8 +16,11 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+app.use(compression);
+
+
 //app.use(express.static(__dirname + '/public'));
-app.use(express.static(__dirname  ));
+app.use(express.static(__dirname   ));
 
 
 app.get('/sss', function(req, res){
