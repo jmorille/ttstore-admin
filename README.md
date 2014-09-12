0632fdfb2d3c400d7d52431c913ffe6553a3256a
jmorille <jmorille@gmail.com>
2014-09-12 22:05:18 +0200

    proxy

--- README.md
+++ README.md
@@ -7,6 +7,10 @@ See the [component page](http://jmorille.github.io/ttstore-admin) for more infor
 
 We've put together a [guide for ttstore-admin](http://www.polymer-project.org/docs/start/reusableelements.html) to help get you rolling.
 
+== Vulcanize
+Issue : https://github.com/Polymer/polymer/issues/706
+
+
 ## Run Local Server
 python -m SimpleHTTPServer
 
--- proxy.js
+++ proxy.js
@@ -5,6 +5,7 @@ var express = require("express"),
     methodOverride = require('method-override'),
     port = parseInt(process.env.PORT, 10) || 8000;
 
+
 // https://github.com/senchalabs/connect#middleware
 var compression = require('compression');
 var serveStatic = require('serve-static');
@@ -19,19 +20,35 @@ app.use(bodyParser.urlencoded({
 
 app.use(compression({
     threshold: 512
-}))
+}));
+
+
+var oneDay = 86400000;
+
+app.route('/components/*')
+    .get(function (req, res, next) {
+        console.log(new Date(Date.now()).toUTCString(),"-", req.method, "-", req.url);
+        res.setHeader("Cache-Control", "public, max-age=" + oneDay); // 1 days
+        res.setHeader("Expires", new Date(Date.now() + oneDay * 1000).toUTCString());
+        res.setHeader("Pragma", "cache");
+        next();
+    });
+
+//app.use(function (req, res, next) {
+//     console.log(new Date(Date.now()).toUTCString(), req.method, req.url);
+//    if(req.url.indexOf("/components/") === 0) {
+//       res.setHeader("Cache-Control", "public, max-age="+oneDay); // 1 days
+//       res.setHeader("Expires", new Date(Date.now() + oneDay*1000).toUTCString());
+//    }
+//    next();
+//});
 
 
 app.use(serveStatic(__dirname + '/back', {  'index': ['index.html', 'index.htm']}))
 
+
 //app.use(express.static(__dirname + '/back'));
 //app.use(express.static(__dirname   ));
-
-
-app.get('/sss', function(req, res){
-    res.redirect('/user/0');
-});
-
 
 
 app.use(errorHandler({
