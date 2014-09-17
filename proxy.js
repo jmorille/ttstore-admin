7eb74798eb598e1f3f8da762581a44688911c5be
jmorille <jmorille@gmail.com>
2014-09-17 20:55:14 +0200

    redirect to es server

--- proxy.js
+++ proxy.js
@@ -10,6 +10,9 @@ var express = require("express"),
 var compression = require('compression');
 var serveStatic = require('serve-static');
 
+var url = require('url');
+var http = require('http');
+
 // https://github.com/strongloop/express/tree/master/examples
 
 app.use(methodOverride());
@@ -27,7 +30,7 @@ var oneDay = 86400000;
 
 app.route('/components/*')
     .get(function (req, res, next) {
-        console.log(new Date(Date.now()).toUTCString(),"-", req.method, "-", req.url);
+        console.log(new Date(Date.now()).toUTCString(), "-", req.method, "-", req.url);
         res.setHeader("Cache-Control", "public, max-age=" + oneDay); // 1 days
         res.setHeader("Expires", new Date(Date.now() + oneDay * 1000).toUTCString());
         res.setHeader("Pragma", "cache");
@@ -45,6 +48,33 @@ app.route('/components/*')
 
 
 app.use(serveStatic(__dirname + '/back', {  'index': ['index.html', 'index.htm']}))
+
+app.route('/es/*')
+    .all(function (request, response, next) {
+        // https://gist.github.com/cmawhorter/a527a2350d5982559bb6
+        console.log('******', new Date(Date.now()).toUTCString(), "-", request.method, "-", request.url);
+        // request.pause(); // Pause the request
+
+        // Construct Redirect
+        var redirectPath = request.url.slice(3);
+        console.log('****** redirect path', redirectPath);
+
+        var options = url.parse("http://192.168.1.100:9200" + redirectPath);
+        // var options = url.parse( "http://localhost:9200"+redirectPath);
+        // console.dir(options);
+        options.headers = request.headers;
+        options.method = request.method;
+        options.agent = false;
+        console.dir(request);
+        // Init connector
+        var connector = http.request(options, function (res) {
+            response.writeHead(res.statusCode, res.headers);
+            res.pipe(response, {end: true });//tell 'response' end=true
+            console.log('    *', new Date(Date.now()).toUTCString(), "-", "statusCode", res.statusCode);
+        });
+        request.pipe(connector, {end: true});
+        // request.resume();// Resume the request
+    });
 
 
 //app.use(express.static(__dirname + '/back'));
