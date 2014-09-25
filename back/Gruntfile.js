4d2512d3e1dde8507a3379a8528f31e3b223899d
jmorille <jmorille@gmail.com>
2014-09-25 17:54:37 +0200

    Table div

--- back/Gruntfile.js
+++ back/Gruntfile.js
@@ -2,7 +2,7 @@
 var LIVERELOAD_PORT = 35729;
 var lrSnippet = require('connect-livereload')({port: LIVERELOAD_PORT});
 var mountFolder = function (connect, dir) {
-  return connect.static(require('path').resolve(dir));
+    return connect.static(require('path').resolve(dir));
 };
 
 // # Globbing
@@ -12,292 +12,310 @@ var mountFolder = function (connect, dir) {
 // 'test/spec/**/*.js'
 
 module.exports = function (grunt) {
-  // show elapsed time at the end
-  require('time-grunt')(grunt);
-  // load all grunt tasks
-  require('load-grunt-tasks')(grunt);
+    // show elapsed time at the end
+    require('time-grunt')(grunt);
+    // load all grunt tasks
+    require('load-grunt-tasks')(grunt);
 
-  // configurable paths
-  var yeomanConfig = {
-    app: 'app',
-    dist: 'dist'
-  };
+    // configurable paths
+    var yeomanConfig = {
+        app: 'app',
+        dist: 'dist'
+    };
 
-  grunt.initConfig({
-    yeoman: yeomanConfig,
-    watch: {
-      options: {
-        nospawn: true,
-        livereload: { liveCSS: false }
-      },
-      livereload: {
-        options: {
-          livereload: true
+    grunt.initConfig({
+        yeoman: yeomanConfig,
+        watch: {
+            options: {
+                nospawn: true,
+                livereload: { liveCSS: false }
+            },
+            livereload: {
+                options: {
+                    livereload: true
+                },
+                files: [
+                    '<%= yeoman.app %>/*.html',
+                    '<%= yeoman.app %>/elements/{,*/}*.html',
+                    '{.tmp,<%= yeoman.app %>}/elements/{,*/}*.css',
+                    '{.tmp,<%= yeoman.app %>}/styles/{,*/}*.css',
+                    '{.tmp,<%= yeoman.app %>}/scripts/{,*/}*.js',
+                    '<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp}'
+                ]
+            },
+            js: {
+                files: ['<%= yeoman.app %>/scripts/{,*/}*.js'],
+                tasks: ['jshint']
+            },
+            styles: {
+                files: [
+                    '<%= yeoman.app %>/styles/{,*/}*.css',
+                    '<%= yeoman.app %>/elements/{,*/}*.css'
+                ],
+                tasks: ['copy:styles', 'autoprefixer:server']
+            }
         },
-        files: [
-          '<%= yeoman.app %>/*.html',
-          '<%= yeoman.app %>/elements/{,*/}*.html',
-          '{.tmp,<%= yeoman.app %>}/elements/{,*/}*.css',
-          '{.tmp,<%= yeoman.app %>}/styles/{,*/}*.css',
-          '{.tmp,<%= yeoman.app %>}/scripts/{,*/}*.js',
-          '<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp}'
-        ]
-      },
-      js: {
-        files: ['<%= yeoman.app %>/scripts/{,*/}*.js'],
-        tasks: ['jshint']
-      },
-      styles: {
-        files: [
-          '<%= yeoman.app %>/styles/{,*/}*.css',
-          '<%= yeoman.app %>/elements/{,*/}*.css'
-        ],
-        tasks: ['copy:styles', 'autoprefixer:server']
-      }
-    },
-    autoprefixer: {
-      options: {
-        browsers: ['last 2 versions']
-      },
-      server: {
-        files: [{
-          expand: true,
-          cwd: '.tmp',
-          src: '**/*.css',
-          dest: '.tmp'
-        }]
-      },
-      dist: {
-        files: [{
-          expand: true,
-          cwd: '<%= yeoman.dist %>',
-          src: ['**/*.css', '!components/**/*.css'],
-          dest: '<%= yeoman.dist %>'
-        }]
-      }
-    },
-    connect: {
-      options: {
-        port: 9000,
-        // change this to '0.0.0.0' to access the server from outside
-        hostname: 'localhost'
-      },
-      livereload: {
-        options: {
-          middleware: function (connect) {
-            return [
-              lrSnippet,
-              mountFolder(connect, '.tmp'),
-              mountFolder(connect, yeomanConfig.app)
-            ];
-          }
-        }
-      },
-      test: {
-        options: {
-          open: {
-            target: 'http://localhost:<%= connect.options.port %>/test'
-          },
-          middleware: function (connect) {
-            return [
-              mountFolder(connect, yeomanConfig.app)
-            ];
-          },
-          keepalive: true
-        }
-      },
-      dist: {
-        options: {
-          middleware: function (connect) {
-            return [
-              mountFolder(connect, yeomanConfig.dist)
-            ];
-          }
-        }
-      }
-    },
-    open: {
-      server: {
-        path: 'http://localhost:<%= connect.options.port %>'
-      }
-    },
-    clean: {
-      dist: ['.tmp', '<%= yeoman.dist %>/*'],
-      server: '.tmp'
-    },
-    jshint: {
-      options: {
-        jshintrc: '.jshintrc',
-        reporter: require('jshint-stylish')
-      },
-      all: [
-        '<%= yeoman.app %>/scripts/{,*/}*.js',
-        '!<%= yeoman.app %>/scripts/vendor/*',
-        'test/spec/{,*/}*.js'
-      ]
-    },
-    useminPrepare: {
-      html: '<%= yeoman.app %>/index.html',
-      options: {
-        dest: '<%= yeoman.dist %>'
-      }
-    },
-    usemin: {
-      html: ['<%= yeoman.dist %>/{,*/}*.html'],
-      css: ['<%= yeoman.dist %>/styles/{,*/}*.css'],
-      options: {
-        dirs: ['<%= yeoman.dist %>'],
-        blockReplacements: {
-          vulcanized: function (block) {
-            return '<link rel="import" href="' + block.dest + '">';
-          }
-        }
-      }
-    },
-    vulcanize: {
-      default: {
-        options: {
-          strip: true
+        autoprefixer: {
+            options: {
+                browsers: ['last 2 versions']
+            },
+            server: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: '.tmp',
+                        src: '**/*.css',
+                        dest: '.tmp'
+                    }
+                ]
+            },
+            dist: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: '<%= yeoman.dist %>',
+                        src: ['**/*.css', '!components/**/*.css'],
+                        dest: '<%= yeoman.dist %>'
+                    }
+                ]
+            }
         },
-        files: {
-          '<%= yeoman.dist %>/elements/elements.vulcanized.html': [
-            '<%= yeoman.dist %>/elements/elements.html'
-          ]
+        connect: {
+            options: {
+                port: 9000,
+                // change this to '0.0.0.0' to access the server from outside
+                hostname: 'localhost'
+            },
+            livereload: {
+                options: {
+                    middleware: function (connect) {
+                        return [
+                            lrSnippet,
+                            mountFolder(connect, '.tmp'),
+                            mountFolder(connect, yeomanConfig.app)
+                        ];
+                    }
+                }
+            },
+            test: {
+                options: {
+                    open: {
+                        target: 'http://localhost:<%= connect.options.port %>/test'
+                    },
+                    middleware: function (connect) {
+                        return [
+                            mountFolder(connect, yeomanConfig.app)
+                        ];
+                    },
+                    keepalive: true
+                }
+            },
+            dist: {
+                options: {
+                    middleware: function (connect) {
+                        return [
+                            mountFolder(connect, yeomanConfig.dist)
+                        ];
+                    }
+                }
+            }
+        },
+        open: {
+            server: {
+                path: 'http://localhost:<%= connect.options.port %>'
+            }
+        },
+        clean: {
+            dist: ['.tmp', '<%= yeoman.dist %>/*'],
+            server: '.tmp'
+        },
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc',
+                reporter: require('jshint-stylish')
+            },
+            all: [
+                '<%= yeoman.app %>/scripts/{,*/}*.js',
+                '!<%= yeoman.app %>/scripts/vendor/*',
+                'test/spec/{,*/}*.js'
+            ]
+        },
+        useminPrepare: {
+            html: '<%= yeoman.app %>/index.html',
+            options: {
+                dest: '<%= yeoman.dist %>'
+            }
+        },
+        usemin: {
+            html: ['<%= yeoman.dist %>/{,*/}*.html'],
+            css: ['<%= yeoman.dist %>/styles/{,*/}*.css'],
+            options: {
+                dirs: ['<%= yeoman.dist %>'],
+                blockReplacements: {
+                    vulcanized: function (block) {
+                        return '<link rel="import" href="' + block.dest + '">';
+                    }
+                }
+            }
+        },
+        vulcanize: {
+            default: {
+                options: {
+                    inline: true,
+                    csp: true,
+                    strip: true,
+                    excludes: {
+                        imports: ['polymer.html']
+                    }
+                },
+                files: {
+                    '<%= yeoman.dist %>/index.html': [ '<%= yeoman.app %>/index.html'
+                    ]
+                }
+            }
+        },
+        imagemin: {
+            dist: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: '<%= yeoman.app %>/images',
+                        src: '{,*/}*.{png,jpg,jpeg}',
+                        dest: '<%= yeoman.dist %>/images'
+                    }
+                ]
+            }
+        },
+        cssmin: {
+            main: {
+                files: {
+                    '<%= yeoman.dist %>/styles/main.css': [
+                        '.tmp/concat/styles/{,*/}*.css'
+                    ]
+                }
+            },
+            elements: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: '.tmp/elements',
+                        src: '{,*/}*.css',
+                        dest: '<%= yeoman.dist %>/elements'
+                    }
+                ]
+            }
+        },
+        minifyHtml: {
+            options: {
+                quotes: true,
+                empty: true
+            },
+            app: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: '<%= yeoman.dist %>',
+                        src: '*.html',
+                        dest: '<%= yeoman.dist %>'
+                    }
+                ]
+            }
+        },
+        copy: {
+            dist: {
+                files: [
+                    {
+                        expand: true,
+                        dot: true,
+                        cwd: '<%= yeoman.app %>',
+                        dest: '<%= yeoman.dist %>',
+                        src: [
+                            '*.{ico,txt}',
+                            '.htaccess',
+                            '*.html',
+                            'elements/**',
+                            '!elements/**/*.css',
+                            'images/{,*/}*.{webp,gif}',
+                            'components/**'
+                        ]
+                    }
+                ]
+            },
+            styles: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: '<%= yeoman.app %>',
+                        dest: '.tmp',
+                        src: ['{styles,elements}/{,*/}*.css']
+                    }
+                ]
+            }
+        },
+        // See this tutorial if you'd like to run PageSpeed
+        // against localhost: http://www.jamescryer.com/2014/06/12/grunt-pagespeed-and-ngrok-locally-testing/
+        pagespeed: {
+            options: {
+                // By default, we use the PageSpeed Insights
+                // free (no API key) tier. You can use a Google
+                // Developer API key if you have one. See
+                // http://goo.gl/RkN0vE for info
+                nokey: true
+            },
+            // Update `url` below to the public URL for your site
+            mobile: {
+                options: {
+                    url: "https://developers.google.com/web/fundamentals/",
+                    locale: "en_GB",
+                    strategy: "mobile",
+                    threshold: 80
+                }
+            }
         }
-      }
-    },
-    imagemin: {
-      dist: {
-        files: [{
-          expand: true,
-          cwd: '<%= yeoman.app %>/images',
-          src: '{,*/}*.{png,jpg,jpeg}',
-          dest: '<%= yeoman.dist %>/images'
-        }]
-      }
-    },
-    cssmin: {
-      main: {
-        files: {
-          '<%= yeoman.dist %>/styles/main.css': [
-            '.tmp/concat/styles/{,*/}*.css'
-          ]
+    });
+
+    grunt.registerTask('server', function (target) {
+        grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
+        grunt.task.run(['serve:' + target]);
+    });
+
+    grunt.registerTask('serve', function (target) {
+        if (target === 'dist') {
+            return grunt.task.run(['build', 'open', 'connect:dist:keepalive']);
         }
-      },
-      elements: {
-        files: [{
-          expand: true,
-          cwd: '.tmp/elements',
-          src: '{,*/}*.css',
-          dest: '<%= yeoman.dist %>/elements'
-        }]
-      }
-    },
-    minifyHtml: {
-      options: {
-        quotes: true,
-        empty: true
-      },
-      app: {
-        files: [{
-          expand: true,
-          cwd: '<%= yeoman.dist %>',
-          src: '*.html',
-          dest: '<%= yeoman.dist %>'
-        }]
-      }
-    },
-    copy: {
-      dist: {
-        files: [{
-          expand: true,
-          dot: true,
-          cwd: '<%= yeoman.app %>',
-          dest: '<%= yeoman.dist %>',
-          src: [
-            '*.{ico,txt}',
-            '.htaccess',
-            '*.html',
-            'elements/**',
-            '!elements/**/*.css',
-            'images/{,*/}*.{webp,gif}',
-            'components/**'
-          ]
-        }]
-      },
-      styles: {
-        files: [{
-          expand: true,
-          cwd: '<%= yeoman.app %>',
-          dest: '.tmp',
-          src: ['{styles,elements}/{,*/}*.css']
-        }]
-      }
-    },
-    // See this tutorial if you'd like to run PageSpeed
-    // against localhost: http://www.jamescryer.com/2014/06/12/grunt-pagespeed-and-ngrok-locally-testing/
-    pagespeed: {
-      options: {
-        // By default, we use the PageSpeed Insights
-        // free (no API key) tier. You can use a Google
-        // Developer API key if you have one. See
-        // http://goo.gl/RkN0vE for info
-        nokey: true
-      },
-      // Update `url` below to the public URL for your site
-      mobile: {
-        options: {
-          url: "https://developers.google.com/web/fundamentals/",
-          locale: "en_GB",
-          strategy: "mobile",
-          threshold: 80
-        }
-      }
-    }
-  });
 
-  grunt.registerTask('server', function (target) {
-    grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
-    grunt.task.run(['serve:' + target]);
-  });
+        grunt.task.run([
+            'clean:server',
+            'copy:styles',
+            'autoprefixer:server',
+            'connect:livereload',
+            'open',
+            'watch'
+        ]);
+    });
 
-  grunt.registerTask('serve', function (target) {
-    if (target === 'dist') {
-      return grunt.task.run(['build', 'open', 'connect:dist:keepalive']);
-    }
-
-    grunt.task.run([
-      'clean:server',
-      'copy:styles',
-      'autoprefixer:server',
-      'connect:livereload',
-      'open',
-      'watch'
+    grunt.registerTask('test', [
+        'clean:server',
+        'connect:test'
     ]);
-  });
 
-  grunt.registerTask('test', [
-    'clean:server',
-    'connect:test'
-  ]);
+    grunt.registerTask('build', [
+        'clean:dist',
+        'copy',
+        'useminPrepare',
+        'imagemin',
+        'concat',
+        'autoprefixer',
+        'uglify',
+        'cssmin',
+        'vulcanize',
+        'usemin',
+        'minifyHtml'
+    ]);
 
-  grunt.registerTask('build', [
-    'clean:dist',
-    'copy',
-    'useminPrepare',
-    'imagemin',
-    'concat',
-    'autoprefixer',
-    'uglify',
-    'cssmin',
-    'vulcanize',
-    'usemin',
-    'minifyHtml'
-  ]);
-
-  grunt.registerTask('default', [
-    'jshint',
-    // 'test'
-    'build'
-  ]);
+    grunt.registerTask('default', [
+        'jshint',
+        // 'test'
+        'build'
+    ]);
 };
