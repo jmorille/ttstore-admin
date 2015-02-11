877d9a1251ec221854890ed6646e154f053bc2a7
jmorille <jmorille@gmail.com>
2015-02-11 10:50:36 +0100

    spa

---
+++ back/gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require('gulp');
+var path = require('path');
+
+var exec = require('gulp-exec');
+
+var vulcanize = require('gulp-vulcanize');
+
+gulp.task('default', ['vulcanize', 'statics']);
+
+gulp.task('vulcanize', function () {
+  var DEST_DIR = 'spa-dist';
+
+  return gulp.src('spa/index.html')
+    .pipe(vulcanize({
+      dest: DEST_DIR,
+      strip: false,
+      inline: true,
+      csp: true
+    }))
+    .pipe(gulp.dest(DEST_DIR));
+});
+
+
+// Copy all static files.
+// TODO: figure out if we can copy less things, especially considering the vulcanized main files.
+gulp.task('statics', function () {
+  staticFiles = [
+    'spa/*.js', 'spa/*.css'
+  ];
+  return gulp.src(staticFiles )
+    .pipe(gulp.dest('./dest-test'));
+});
+
+
+// cca create
+gulp.task('cca-create', function(cb) {
+  gulp
+    .src('.').pipe(exec("cca create spa-mobile --link-to=spa-dist/manifest.json", function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    cb(err);
+  }));
+});
+
+
