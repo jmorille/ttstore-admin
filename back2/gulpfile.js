5403cec03e4b7ecd43dac9c931cea0e990f21389
jmorille <jmorille@gmail.com>
2015-02-13 21:19:52 +0100

    sync tasks

--- back2/gulpfile.js
+++ back2/gulpfile.js
@@ -18,14 +18,17 @@ var vulcanize = require('gulp-vulcanize');
 
 // Command line conf
 var gutil = require('gulp-util'),
-  prod  = gutil.env.prod;
+  prod = gutil.env.prod;
 
 // Browser reload
 var webserver = require('gulp-webserver');
-var opn       = require('opn');
+var opn = require('opn');
 
 // Mobile
 var exec = require('gulp-exec');
+var shell = require('gulp-shell');
+
+var debug = require('gulp-debug');
 
 // Config
 var path = {
@@ -52,16 +55,15 @@ gulp.task('clean', ['clean:css'], function () {
 });
 
 gulp.task('clean:css', function () {
-  return gulp.src(['app/**/*.css'], {read: false}) .pipe(clean());
+  return gulp.src(['app/**/*.css', '!app/bower_components/**/*.*'], {read: false}).pipe(clean());
 });
 
 
-
-
-gulp.task('build', function(callback) {
-  runSequence('sass',  'vulcanize',  callback);
+gulp.task('build', function (callback) {
+  return runSequence('sass', 'vulcanize', callback);
 });
 
+gulp.task('bb', ['sass', 'vulcanize']);
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Browsers TASKS
@@ -71,21 +73,20 @@ var server = {
   port: '8001'
 }
 
-gulp.task('webserver', ['watch'], function() {
-  gulp.src(path.build_web)
+gulp.task('webserver', ['watch'], function () {
+  return gulp.src(path.build_web)
     .pipe(webserver({
-      host:             server.host,
-      port:             server.port,
+      host: server.host,
+      port: server.port,
       livereload: true,
       directoryListing: false,
       open: gutil.env.open
     }));
 });
 
-gulp.task('openbrowser', function() {
-  opn( 'http://' + server.host + ':' + server.port );
+gulp.task('openbrowser', function () {
+  return opn('http://' + server.host + ':' + server.port);
 });
-
 
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -105,19 +106,16 @@ gulp.task('lint', function () {
 
 gulp.task('sass', function () {
   var DEST_DIR = path.app;
-  var SASS_OPTS = prod ? {
-    outputStyle: 'compressed',
-    sourceComments: 'map'
-  } : {};
-  gulp.src(path.sass, {cwd: path.app})
+  var SASS_OPTS = prod ? { outputStyle: 'compressed'} : {};
+  return gulp.src(path.sass, {cwd: path.app})
     .pipe(sass(SASS_OPTS))
     // Pass the compiled sass through the prefixer with defined
     //.pipe(prefix(
     //  'last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'
     //))
     .pipe(gulp.dest(DEST_DIR));
-
   //gulp.src(path.sass_not, {cwd: path.app}).pipe(gulp.dest(DEST_DIR));
+
 
 });
 
@@ -126,7 +124,6 @@ gulp.task('sass', function () {
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 gulp.task('vulcanize', function () {
   var DEST_DIR = path.build_web;
-
   return gulp.src('index.html', {cwd: path.app})
     .pipe(vulcanize({
       dest: DEST_DIR,
@@ -134,73 +131,80 @@ gulp.task('vulcanize', function () {
       inline: true,
       csp: true
     }))
-    .pipe(gulp.dest(DEST_DIR)) ;
+    .pipe(gulp.dest(DEST_DIR));
 });
 
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// CCA TASKS
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-gulp.task('chromeapp:cp', function () {
- return gulp.src(path.src_chromeapp+"/**")
-   .pipe(gulp.dest(path.build_web)) ;
-});
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Watch TASKS
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-gulp.task('watch',  [ 'sass', 'vulcanize'], function () {
+gulp.task('watch', ['sass', 'vulcanize'], function (cb) {
   gulp.watch(path.sass, ['sass', 'vulcanize']);
   gulp.watch(path.src_polymer, ['vulcanize']);
-
+  cb();
 });
 
 
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//  Chrome App TASKS
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+gulp.task('chromeapp:cp', function () {
+  return gulp.src(path.src_chromeapp + "/**")
+    .pipe(gulp.dest(path.build_web));
+});
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Chrome App Mobile TASKS
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-gulp.task('cca:create', function (cb) {
-  var cca_action = ' --link-to=';
-  //var cca_action = ' --copy-from=';
-  gulp.src('.')
-    .pipe(exec('cca create '+ path.build_cca + cca_action + path.build_web +  '/manifest.json', function (err, stdout, stderr) {
-      gutil.log(  gutil.colors.cyan(stdout));
-      gutil.log(  gutil.colors.red(stderr));
-     // console.log(stdout);
+var consolePrinter = function (cb) {
+  return function (err, stdout, stderr) {
+    gutil.log(gutil.colors.cyan(stdout));
+    gutil.log(gutil.colors.red(stderr));
+    // console.log(stdout);
     // console.log(stderr);
-      cb(err);
-    }));
+    cb();
+  };
+};
+
+
+gulp.task('cca:create', function (cb) {
+  //var cca_action = ' --link-to=';
+  var cca_action = ' --copy-from=';
+  gulp.src('.')
+    .pipe(exec('cca create ' + path.build_cca + cca_action + path.build_web + '/manifest.json', consolePrinter(cb) ));
 });
 
 gulp.task('cca:checkenv', function (cb) {
-  gulp.src( path.build_cca)
-    .pipe(exec('cca checkenv', function (err, stdout, stderr) {
-      gutil.log(  stdout);
-      gutil.log(  gutil.colors.red(stderr));
-      // console.log(stdout);
-      // console.log(stderr);
-      cb(err);
-    }));
+  gulp.src(path.build_cca)
+    .pipe(exec('cca checkenv', consolePrinter(cb)));
 });
 
 gulp.task('cca:push', function (cb) {
-  gulp.src( path.build_cca , {cwd: path.build_cca } )
-    .pipe(exec('cca push', function (err, stdout, stderr) {
-      gutil.log(  stdout);
-      gutil.log(  gutil.colors.red(stderr));
-      // console.log(stdout);
-      // console.log(stderr);
-      cb(err);
-    }));
+  gulp.src(path.build_cca, {cwd: path.build_cca})
+    .pipe(exec('cca push', consolePrinter(cb) ));
 });
 
+
 gulp.task('cca:build', function (cb) {
-  gulp.src( path.build_cca , {cwd: path.build_cca } )
-    .pipe(exec('cca build android', function (err, stdout, stderr) {
-      gutil.log(  stdout);
-      gutil.log(  gutil.colors.red(stderr));
+  gulp.src( '*', {read: false, cwd: path.build_cca+'/'})
+    .pipe(debug({title: 'unicorn:'}))
+    .pipe(exec('cca build android', consolePrinter(cb)));
+});
+
+gulp.task('pwd', function (cb) {
+  gulp.src(path.build_cca, {cwd: path.build_cca})
+    .pipe(exec('pwd', function (err, stdout, stderr) {
+      gutil.log(stdout);
+      gutil.log(gutil.colors.red(stderr));
+      console.log('Current directory: ' + process.cwd());
       // console.log(stdout);
       // console.log(stderr);
-      cb(err);
+      //cb(err);
     }));
 });
+gulp.task('shorthand', shell.task([
+  'pwd',
+  'ls ' + path.build_cca,
+  'pwd'
+], {cwd: path.build_cca}));
