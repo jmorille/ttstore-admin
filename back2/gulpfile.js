190478f5a40154dc485edb2abf225d2618238696
jmorille <jmorille@gmail.com>
2015-02-13 17:31:25 +0100

    gulp

--- back2/gulpfile.js
+++ back2/gulpfile.js
@@ -5,17 +5,37 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
 
+// Cache
+var cache = require('gulp-cached'),
+  remember = require('gulp-remember');
+
 // Build
 var clean = require('gulp-clean');
+var runSequence = require('run-sequence');
 var sass = require('gulp-sass');
+//var prefix = require('gulp-autoprefixer');
 var vulcanize = require('gulp-vulcanize');
 
+// Command line conf
+var gutil = require('gulp-util'),
+  prod  = gutil.env.prod;
+
+// Browser reload
+var webserver = require('gulp-webserver');
+var opn       = require('opn');
+
+// Mobile
+var exec = require('gulp-exec');
 
 // Config
 var path = {
   app: 'app',
-  build: 'build',
+  src_chromeapp: 'chromeapp',
+  build_web: 'build/web',
+  build_chromeapp: 'build/chromeapp',
+  build_cca: 'build/cca',
   dist: 'dist',
+  src_polymer: ['app/elements/**/*.html'],
   sources: ['app/elements/**/*.html', 'app/scripts/{,*/}*.js'],
   sass: ['**/*.{scss,sass}', '!bower_components/**'],
   sass_not: ['**', '!**/*.{scss,sass}']
@@ -26,10 +46,46 @@ var path = {
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 gulp.task('default', ['lint']);
 
-gulp.task('clean', function () {
-  return gulp.src([path.build, path.dist], {read: false})
+gulp.task('clean', ['clean:css'], function () {
+  return gulp.src(['build', 'dist'], {read: false})
     .pipe(clean()); // Delete dist and build to allow for nice, clean files!
 });
+
+gulp.task('clean:css', function () {
+  return gulp.src(['app/**/*.css'], {read: false}) .pipe(clean());
+});
+
+
+
+
+gulp.task('build', function(callback) {
+  runSequence('sass',  'vulcanize',  callback);
+});
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Browsers TASKS
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+var server = {
+  host: 'localhost',
+  port: '8001'
+}
+
+gulp.task('webserver', ['watch'], function() {
+  gulp.src(path.build_web)
+    .pipe(webserver({
+      host:             server.host,
+      port:             server.port,
+      livereload: true,
+      directoryListing: false,
+      open: gutil.env.open
+    }));
+});
+
+gulp.task('openbrowser', function() {
+  opn( 'http://' + server.host + ':' + server.port );
+});
+
 
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -48,12 +104,20 @@ gulp.task('lint', function () {
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 gulp.task('sass', function () {
+  var DEST_DIR = path.app;
+  var SASS_OPTS = prod ? {
+    outputStyle: 'compressed',
+    sourceComments: 'map'
+  } : {};
   gulp.src(path.sass, {cwd: path.app})
-    .pipe(sass())
-    .pipe(gulp.dest(path.build));
+    .pipe(sass(SASS_OPTS))
+    // Pass the compiled sass through the prefixer with defined
+    //.pipe(prefix(
+    //  'last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'
+    //))
+    .pipe(gulp.dest(DEST_DIR));
 
-  gulp.src(path.sass_not, {cwd: path.app})
-    .pipe(gulp.dest(path.build));
+  //gulp.src(path.sass_not, {cwd: path.app}).pipe(gulp.dest(DEST_DIR));
 
 });
 
@@ -61,23 +125,33 @@ gulp.task('sass', function () {
 // Vulcanize TASKS
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 gulp.task('vulcanize', function () {
-  var DEST_DIR = path.dist;
+  var DEST_DIR = path.build_web;
 
-  return gulp.src('index.html', {cwd: path.build})
+  return gulp.src('index.html', {cwd: path.app})
     .pipe(vulcanize({
       dest: DEST_DIR,
-      strip: false,
+      strip: prod,
       inline: true,
       csp: true
     }))
-    .pipe(gulp.dest(DEST_DIR));
+    .pipe(gulp.dest(DEST_DIR)) ;
+});
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// CCA TASKS
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+gulp.task('chromeapp:cp', function () {
+ return gulp.src(path.src_chromeapp+"/**")
+   .pipe(gulp.dest(path.build_web)) ;
 });
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Watch TASKS
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-gulp.task('watch', function () {
-  gulp.watch(path.sources, ['sass', 'vulcanize']);
+gulp.task('watch',  [ 'sass', 'vulcanize'], function () {
+  gulp.watch(path.sass, ['sass', 'vulcanize']);
+  gulp.watch(path.src_polymer, ['vulcanize']);
+
 });
 
 
@@ -85,11 +159,24 @@ gulp.task('watch', function () {
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Chrome App TASKS
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-gulp.task('cca-create', function (cb) {
+gulp.task('cca:create', function (cb) {
   gulp.src('.')
-    .pipe(exec('cca create spa-mobile --link-to=spa-dist/manifest.json', function (err, stdout, stderr) {
-      console.log(stdout);
-      console.log(stderr);
+    .pipe(exec('cca create '+ path.build_cca + ' --link-to=' + path.build_web +  '/manifest.json', function (err, stdout, stderr) {
+      gutil.log(  gutil.colors.cyan(stdout));
+      gutil.log(  gutil.colors.red(stderr));
+     // console.log(stdout);
+    // console.log(stderr);
+      cb(err);
+    }));
+});
+
+gulp.task('cca:checkenv', function (cb) {
+  gulp.src( path.build_cca)
+    .pipe(exec('cca checkenv', function (err, stdout, stderr) {
+      gutil.log(  stdout);
+      gutil.log(  gutil.colors.red(stderr));
+      // console.log(stdout);
+      // console.log(stderr);
       cb(err);
     }));
 });
