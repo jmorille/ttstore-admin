9b38d1fc5a533b87a4c65c8ea6e688def4d0c079
jmorille <jmorille@gmail.com>
2015-05-04 22:08:49 +0200

    verify token

--- hapi/src/routes/auth.js
+++ hapi/src/routes/auth.js
@@ -3,8 +3,17 @@
 var Boom = require('boom'); // error handling https://github.com/hapijs/boom
 var bcrypt = require('bcrypt'); // import the module we are using to encrypt passwords
 
-var JwtSign = require('../security/auth_jwt_sign.js');
+var jwtSign = require('../security/auth_jwt_sign.js');
 var userController = require('./../controllers/user');
+
+var google = require('googleapis');
+//google.options({ proxy: 'http://proxy.example.com', auth: auth });
+
+var plus = google.plus('v1');
+var OAuth2 = google.auth.OAuth2;
+var oauth2 = google.oauth2('v2');
+var Config = require("../config");
+var GoogleProviders = Config.get('/provider').google;
 
 // https://github.com/ideaq/hapi-auth-jwt2/blob/master/example/simple_server.js
 module.exports = function (plugin, options, next) {
@@ -41,13 +50,59 @@ module.exports = function (plugin, options, next) {
         console.log('Login  Request Auth ', request.auth);
         //reply({text: 'You Succefully Login and Attach a JWT token '})
         //  .header("Authorization", request.headers.authorization);
-        JwtSign(request, function (token, res) {
+        jwtSign(request, function (token, res) {
           return reply(res).header("Authorization", token);
         });
       },
       config: {
         auth: 'basic',
         tags: ['api', 'login'],
+        description: 'Login an User'
+      }
+    },
+    {
+      method: 'POST',
+      path: '/s/login/google',
+      handler: function (request, reply) {
+        // https://github.com/google/google-api-nodejs-client
+        console.log('Login  Request Paylaod ', request.payload);
+        var tokens = request.payload;
+        //reply({text: 'You Succefully Login and Attach a JWT token '})
+        //  .header("Authorization", request.headers.authorization);
+        var accessToken = tokens.access_token,
+          refreshToken = tokens.code || null; // refresh_token
+
+
+        var oauth2Client = new OAuth2(GoogleProviders.clientId, GoogleProviders.clientSecret, GoogleProviders.redirectUrl);
+        oauth2Client.setCredentials({
+          access_token: accessToken,
+          refresh_token: refreshToken
+        });
+        oauth2.userinfo.get({
+          auth: oauth2Client
+        }, function (err, profile) {
+          console.log('Google userinfo error', err);
+          console.log('Google userinfo profile', profile);
+        });
+        plus.people.get({userId: 'me', auth: oauth2Client}, function (err, response) {
+          console.log('Google people error', err);
+          console.log('Google people profile', response);
+        });
+        oauth2.tokeninfo({
+          access_token: tokens.access_token,
+          auth: oauth2Client
+        }, function (err, tokens) {
+          console.log('tokeninfo error', err);
+          console.log('tokeninfo', tokens);
+        });
+
+        //jwtSign(request, function (token, res) {
+        //  return reply(res).header("Authorization", token);
+        //});
+      },
+      config: {
+        auth: false,
+        tags: ['api', 'login', 'googleapi'],
         description: 'Login an User'
       }
     },
